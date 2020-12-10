2446b483c6f25f42ec222836b0417e12253d207b
JDiggyDawg <39223336+JDiggyDawg@users.noreply.github.com>
2020-12-09 22:33:56 -0700

    disk_checker_initialize

--- plex_automate.py
+++ plex_automate.py
@@ -12,4 +12,18 @@ def app_checker():
         print("Plex Media Server is missing.")
         exit()
 
-# os.system("pkill Plex\ Media\ Server")
+
+def disk_checker_initialize():
+    disk_exists = os.path.exists("/dev/disk3")
+    if disk_exists is True:
+        print("Disk exists. Ready to start program.")
+        os.system("open /Applications/Plex\ Media\ Server.app")
+    else:
+        os.system("pkill Plex Media Server")
+
+
+app_checker()
+
+disk_check = os.path.ismount("/dev/disk1s1")
+print(disk_check)
+# disk_checker_initialize()
